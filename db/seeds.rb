64a896f154bd889de0713577ecd2035aff41f9e8
Neil Billingham <neil.billingham@me.com>
2014-02-24 16:45:07 +0000

    seed data and precompile option in application.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Location.create(address: "NW3 4BG")
+Location.create(address: "NW3 4BG")
+Location.create(address: "NW3 2AG")
+
