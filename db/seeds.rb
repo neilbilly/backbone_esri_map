1404da51a4cc33998211a35cb2aa7dd3bcff78bd
Neil Billingham <neil.billingham@me.com>
2014-02-24 16:50:33 +0000

    typo in seed data fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Location.create(address: "NW3 4BG")
-Location.create(address: "NW3 4BG")
+Location.create(address: "NW3 4BL")
 Location.create(address: "NW3 2AG")
 
